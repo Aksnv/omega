b31583efe43419eb790daced92df0bcd4a9d8585
Yuri Aksenov <aksenov.mailbox@gmail.com>
2016-12-29 14:13:29 +0300

    Добавлены стили и написан скрипт для проигрывания видео

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
-alert("hello!");
+$(".zp-closed__video-link a").click(function() {
+  $(".zp-closed__hello").fadeOut();
+  $(".zp-closed__pleasure").fadeOut();
+  $(".zp-closed__content").fadeOut();
+  $(".zp-closed__sale").fadeOut();
+  $(".zp-closed__video-link").fadeOut();
+  $(".zp-closed__video").show();
+
+  setTimeout(videoButton, 1000);
+
+  function videoButton() {
+    $(".zp-closed__video-button").css("display", "block");
+  }
+})
+
+$(".zp-closed__video-button").click(function() {
+  $(".zp-closed__hello").fadeIn();
+  $(".zp-closed__pleasure").fadeIn();
+  $(".zp-closed__content").fadeIn();
+  $(".zp-closed__sale").fadeIn();
+  $(".zp-closed__video-link").fadeIn();
+  $(".zp-closed__video").hide();
+});
+
